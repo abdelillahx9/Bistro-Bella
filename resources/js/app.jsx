f533ed2afbd92a8fd26b97d3602d22c261fae91f
abdelillahx9 <abdelillahx9@gmail.com>
2025-12-16 19:57:33 +0100

    make some fonts

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,7 @@ import '../css/app.css';
 import './bootstrap';
 
 // PrimeReact imports
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/themes/saga-orange/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
